d3c19c606a471dfbfdec08a4a19cb4c0fd953356
alpha-live <alpha-live@alpha>
2019-11-21 19:22:38 +0800

    add docs

---
+++ docs/precache-manifest.9dcd19822d4be90dfd27b0df17ccc742.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bec541d51484c94dd647aa57a59feee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0df4a0b47376d46bd35d",
+    "url": "./static/css/2.2ceef76d.chunk.css"
+  },
+  {
+    "revision": "cef8295f3dedacbc9ff7",
+    "url": "./static/css/main.b377cbc2.chunk.css"
+  },
+  {
+    "revision": "0df4a0b47376d46bd35d",
+    "url": "./static/js/2.f864b802.chunk.js"
+  },
+  {
+    "revision": "cef8295f3dedacbc9ff7",
+    "url": "./static/js/main.24ad3a0d.chunk.js"
+  },
+  {
+    "revision": "8d19853fd77a26c52662",
+    "url": "./static/js/runtime-main.933cc3a5.js"
+  },
+  {
+    "revision": "1f8421d005799724cdff8a06547c6733",
+    "url": "./static/media/Alpha1_02.1f8421d0.jpg"
+  },
+  {
+    "revision": "9d24b3428c5a1940c7902ec2f4ce7318",
+    "url": "./static/media/Alpha1_05.9d24b342.jpg"
+  }
+]);
